117375c476185abdc3c54a2fcc21a7e52922d171
Brad Grantham <grantham@plunk.org>
2020-04-07 00:24:03 -0700

    limit rows and make more columns

--- firmware/clocks.py
+++ firmware/clocks.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
-HSE = 16000000
+HSE = 16002676.76554237158493229676 # 16000000
 
 clocksByConfig = []
+
+minAcceptableMHz = 140
+maxAcceptableMHz = 240
 
 for PLLM in range(2, 64):
     # VCO input frequency = PLL input clock frequency / PLLM with 2 <= PLLM <= 63
     VCO_INPUT = HSE / PLLM
     for PLLN in range(50, 432):
         VCO_OUTPUT = VCO_INPUT * PLLN
-        for PLLP in (2, 4, 6, 8):
+        for PLLP in (2,): # (2, 4, 6, 8):
             PLL_OUTPUT = VCO_OUTPUT / PLLP
-            clocksByConfig.append((PLL_OUTPUT, HSE, PLLM, PLLN, PLLP))
+            if PLL_OUTPUT > minAcceptableMHz * 1000000 and PLL_OUTPUT < maxAcceptableMHz * 1000000:
+                DMA_BEATS = int(PLL_OUTPUT / 14318180)
+                for dbeats in (0, 1):
+                    error = (PLL_OUTPUT / (DMA_BEATS + dbeats) - 14318180) / 14318180
+                    actual = PLL_OUTPUT / (DMA_BEATS + dbeats) / 4
+                    clocksByConfig.append((PLL_OUTPUT, HSE, PLLM, PLLN, PLLP, DMA_BEATS + dbeats, actual, error))
 
-print("PLL_OUTPUT MHz, HSE, PLLM, PLLN, PLLP")
-for (PLL_OUTPUT, HSE, PLLM, PLLN, PLLP) in clocksByConfig:
-    print("%f, %d, %d, %d, %d" % (PLL_OUTPUT / 1000000, HSE, PLLM, PLLN, PLLP))
+clocksByConfig.sort(key=lambda elem: elem[6])
+
+print("PLL_OUTPUT MHz, HSE, PLLM, PLLN, PLLP, DMA_BEATS, actual color clock MHz, error")
+for (PLL_OUTPUT, HSE, PLLM, PLLN, PLLP, DMA_BEATS, actual, error) in clocksByConfig:
+    print("%.2f, %d, %d, %d, %d, %d, %f, %f" % (PLL_OUTPUT / 1000000.0, HSE, PLLM, PLLN, PLLP, DMA_BEATS, actual / 1000000.0, error))
