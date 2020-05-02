4f1cc302d405a42a8aa01c751b425ab0325c5d7f
Brad Grantham <grantham@plunk.org>
2020-05-01 22:11:44 -0700

    update to match TIM1 clock in STM324F746

--- firmware/clocks.py
+++ firmware/clocks.py
@@ -5,8 +5,8 @@ HSE = 16000000
 
 clocksByConfig = []
 
-minAcceptableMHz = 100
-maxAcceptableMHz = 225
+minAcceptableMHz = 180
+maxAcceptableMHz = 220
 
 for PLLM in range(2, 64):
     # VCO input frequency = PLL input clock frequency / PLLM with 2 <= PLLM <= 63
@@ -16,15 +16,16 @@ for PLLM in range(2, 64):
         for PLLP in (2,): # (2, 4, 6, 8):
             PLL_OUTPUT = VCO_OUTPUT / PLLP
             if PLL_OUTPUT > minAcceptableMHz * 1000000 and PLL_OUTPUT < maxAcceptableMHz * 1000000:
-                DMA_BEATS = int(PLL_OUTPUT / 14318180)
+                TIM_CLOCKS = int(PLL_OUTPUT / (14318180 * 2))
                 for dbeats in (0, 1):
-                    error = (PLL_OUTPUT / (DMA_BEATS + dbeats) - 14318180) / 14318180
-                    actual = PLL_OUTPUT / (DMA_BEATS + dbeats) / 4
-                    if actual / 3579545 < 1.05 and 379545 / actual < 1.05:
-                        clocksByConfig.append((PLL_OUTPUT, HSE, PLLM, PLLN, PLLP, DMA_BEATS + dbeats, actual, error))
+                    error = (PLL_OUTPUT / ((TIM_CLOCKS + dbeats) * 2) - 14318180) / 14318180
+                    actual = PLL_OUTPUT / ((TIM_CLOCKS + dbeats) * 2 ) / 4
+                    if True: # actual / 3579545 < 1.05 and 3579545 / actual < 1.05:
+                        clocksByConfig.append((PLL_OUTPUT, HSE, PLLM, PLLN, PLLP, TIM_CLOCKS + dbeats, actual, error))
 
-clocksByConfig.sort(key=lambda elem: abs(elem[7]))
+# clocksByConfig.sort(key=lambda elem: abs(elem[7]))
+clocksByConfig.sort(key=lambda elem: elem[7])
 
-print("PLL_OUTPUT MHz, HSE, PLLM, PLLN, PLLP, DMA_BEATS, actual color clock MHz, error")
-for (PLL_OUTPUT, HSE, PLLM, PLLN, PLLP, DMA_BEATS, actual, error) in clocksByConfig:
-    print("%.2f, %d, %d, %d, %d, %d, %f, %f" % (PLL_OUTPUT / 1000000.0, HSE, PLLM, PLLN, PLLP, DMA_BEATS, actual / 1000000.0, error))
+print("PLL_OUTPUT MHz, HSE, PLLM, PLLN, PLLP, TIM_CLOCKS, actual color clock MHz, error")
+for (PLL_OUTPUT, HSE, PLLM, PLLN, PLLP, TIM_CLOCKS, actual, error) in clocksByConfig:
+    print("%.2f, %d, %d, %d, %d, %d, %f, %f" % (PLL_OUTPUT / 1000000.0, HSE, PLLM, PLLN, PLLP, TIM_CLOCKS, actual / 1000000.0, error))
