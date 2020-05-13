1bb3d26f2868bbe46e25ff879e736fc976bf606d
Brad Grantham <grantham@plunk.org>
2020-05-12 18:13:10 -0700

    make new segment utilities

---
+++ firmware/segment_utility.h
@@ -0,0 +1,36 @@
+#ifndef _SEGMENT_UTILITY_H_
+#define _SEGMENT_UTILITY_H_
+
+#include "videomode.h"
+
+void DumpSegment(VideoSegmentedScanlineSegment *segment, int currentStart, int indent);
+
+void DumpSegments(VideoSegmentedScanlineSegment *segment, int pixelCount, int indent);
+
+void SetSegment(VideoSegmentedScanlineSegment *seg, int pixelCount, float r0, float g0, float b0, float r1, float g1, float b1);
+
+int MergeSegment(VideoSegmentedScanlineSegment *newSegment, int start, VideoSegmentedScanlineSegment *oldSegments, int pixelCount, VideoSegmentedScanlineSegment *resultSegments, int maxNewSegmentCount, int *newCount);
+
+typedef struct VideoSegmentBuffer {
+    int scanlineWidth;  // pixels in scanline
+    int segmentsInPool; // Total number of segments allocated
+    VideoSegmentedScanlineSegment *segmentPool;
+    int scanlineCount;  // Total number of scanlines allocated
+    VideoSegmentedScanline *scanlines;
+    int rowBeingUpdated;
+    VideoSegmentedScanlineSegment *currentSegmentDestination;
+    VideoSegmentedScanlineSegment *segmentCeiling;
+} VideoSegmentBuffer;
+
+int VideoBufferAllocateMembers(VideoSegmentBuffer *buffer, int width, int totalSegments, int scanlineCount, float r, float g, float b);
+
+void VideoBufferFreeMembers(VideoSegmentBuffer *buffer);
+
+int VideoBufferBeginUpdate(VideoSegmentBuffer *buffer);
+
+int VideoBufferGetCurrentRowForUpdate(VideoSegmentBuffer *buffer, VideoSegmentedScanlineSegment** curSegments, int *segmentCount);
+
+// Updates current row, increments current row
+int VideoBufferUpdateRow(VideoSegmentBuffer *buffer, VideoSegmentedScanlineSegment* newSegments, int newSegmentCount);
+
+#endif /* _SEGMENT_UTILITY_H_ */
