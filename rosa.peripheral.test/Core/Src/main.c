38cc3affabf92bc5254a620264ff260c845d5f2e
Brad Grantham <grantham@plunk.org>
2020-11-06 13:09:20 -0800

    configured HSE for 25MHz, PLL output at 480MHz

--- rosa.peripheral.test/Core/Src/main.c
+++ rosa.peripheral.test/Core/Src/main.c
@@ -24,6 +24,7 @@
 /* USER CODE BEGIN Includes */
 
 #include <string.h>
+#include <stdio.h>
 
 /* USER CODE END Includes */
 
@@ -45,6 +46,8 @@
 
 UART_HandleTypeDef huart2;
 
+SDRAM_HandleTypeDef hsdram1;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -53,6 +56,7 @@ UART_HandleTypeDef huart2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_FMC_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -68,6 +72,90 @@ void panic()
         HAL_Delay(200);
         HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
     }
+}
+
+uint32_t getMicros(void)
+{
+    uint32_t micros = HAL_RCC_GetSysClockFreq() / 1000000;
+
+    register uint32_t ms, cycle_cnt;
+    do {
+        ms = HAL_GetTick();
+        cycle_cnt = SysTick->VAL;
+    } while (ms != HAL_GetTick());
+    return (ms * 1000) + (micros * 1000 - cycle_cnt) / micros;
+}
+
+void delayMicros(uint16_t micros)
+{
+    uint32_t start = getMicros();
+    while (getMicros()-start < (uint32_t) micros) {
+        asm("nop");
+    }
+}
+
+void delayNanos(uint16_t nanos)
+{
+    uint32_t then = SysTick->VAL;
+    uint32_t nanosPerCycle = 1000000000 / HAL_RCC_GetSysClockFreq();
+    uint32_t cycles = nanos / nanosPerCycle;
+    uint32_t elapsedCycles;
+    do {
+        uint32_t now = SysTick->VAL;
+        elapsedCycles = (now - then);
+    } while(elapsedCycles < cycles);
+}
+
+void WS2812_byte_to_SPI_bytes(uint8_t ws2812, uint32_t *spi)
+{
+    *spi = 0;
+    for(int i = 0; i < 8; i++) {
+        int bits[3];
+        if(ws2812 & (1 << (7 - i))) {
+            bits[0] = 1; bits[1] = 1; bits[2] = 0;
+        } else {
+            bits[0] = 1; bits[1] = 0; bits[2] = 0;
+        }
+        for(int j = 0; j < 3; j++) {
+            int bit = (i * 3 + j);
+            *spi |= (bits[j] << bit);
+        }
+    }
+}
+
+void write24Bits(uint32_t bits)
+{
+    for(int i = 0; i < 24; i++) {
+        int bit = bits & (1 << i);
+        HAL_GPIO_WritePin(RGBLED_GPIO_Port, RGBLED_Pin, bit ? GPIO_PIN_SET : GPIO_PIN_RESET);
+        delayNanos(400);
+        if(0) {
+            if(bit) {
+                HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_SET);
+                HAL_Delay(900);
+                HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
+                HAL_Delay(100);
+            } else {
+                HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_SET);
+                HAL_Delay(100);
+                HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
+                HAL_Delay(900);
+            }
+        }
+    }
+}
+
+void writeColor(uint8_t r, uint8_t g, uint8_t b)
+{
+    uint32_t spi;
+    WS2812_byte_to_SPI_bytes(g, &spi);
+    write24Bits(spi);
+    WS2812_byte_to_SPI_bytes(r, &spi);
+    write24Bits(spi);
+    WS2812_byte_to_SPI_bytes(b, &spi);
+    write24Bits(spi);
+    HAL_GPIO_WritePin(RGBLED_GPIO_Port, RGBLED_Pin, GPIO_PIN_RESET);
+    delayMicros(50);
 }
 
 /* USER CODE END 0 */
@@ -101,6 +189,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART2_UART_Init();
+  MX_FMC_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -108,15 +197,43 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-  int i = 0;
-    const char *message = "Hello World\n";
+    static char message[512];
+    sprintf(message, "Hello World\n");
+    if(HAL_UART_Transmit_IT(&huart2, (uint8_t *)message, strlen(message)) != HAL_OK) {
+        panic();
+    }
+    HAL_Delay(200);
+
+    sprintf(message, "System clock is %lu\n", HAL_RCC_GetSysClockFreq());
+    if(HAL_UART_Transmit_IT(&huart2, (uint8_t *)message, strlen(message)) != HAL_OK) {
+        panic();
+    }
+    HAL_Delay(200);
+
+    uint32_t bits;
+    WS2812_byte_to_SPI_bytes(0xA5, &bits);
+    // 6592CB -> 0110 0101 1001 0010 1100 1011
+    // -> 110100110100100110100110
+    // A5 is 10100101, should be 110100110100100110100110
+    sprintf(message, "bits would become %08lX\n", bits);
     if(HAL_UART_Transmit_IT(&huart2, (uint8_t *)message, strlen(message)) != HAL_OK) {
         panic();
     }
 
+    while(1) {
+        for(int i = 0; i < 1000000; i++) {
+            delayNanos(1000);
+        }
+        HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_SET );
+        for(int i = 0; i < 1000000; i++) {
+            delayNanos(1000);
+        }
+        HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET );
+    }
+
+  int LEDvalue = 255;
   while (1)
   {
-      static char message[512];
       int lightLED = 0;
     /* USER CODE END WHILE */
 
@@ -146,6 +263,9 @@ int main(void)
         lightLED = 1;
     }
     HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, lightLED ? GPIO_PIN_SET : GPIO_PIN_RESET);
+
+    writeColor(LEDvalue, LEDvalue, LEDvalue);
+    LEDvalue = (LEDvalue + 1) % 256;
   }
   /* USER CODE END 3 */
 }
@@ -165,16 +285,24 @@ void SystemClock_Config(void)
   HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
   /** Configure the main internal regulator output voltage
   */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
   while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 10;
+  RCC_OscInitStruct.PLL.PLLN = 384;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_1;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -184,19 +312,20 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
                               |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV1;
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
   {
     Error_Handler();
   }
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART2|RCC_PERIPHCLK_FMC;
+  PeriphClkInitStruct.FmcClockSelection = RCC_FMCCLKSOURCE_D1HCLK;
   PeriphClkInitStruct.Usart234578ClockSelection = RCC_USART234578CLKSOURCE_D2PCLK1;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
   {
@@ -252,6 +381,53 @@ static void MX_USART2_UART_Init(void)
 
 }
 
+/* FMC initialization function */
+static void MX_FMC_Init(void)
+{
+
+  /* USER CODE BEGIN FMC_Init 0 */
+
+  /* USER CODE END FMC_Init 0 */
+
+  FMC_SDRAM_TimingTypeDef SdramTiming = {0};
+
+  /* USER CODE BEGIN FMC_Init 1 */
+
+  /* USER CODE END FMC_Init 1 */
+
+  /** Perform the SDRAM1 memory initialization sequence
+  */
+  hsdram1.Instance = FMC_SDRAM_DEVICE;
+  /* hsdram1.Init */
+  hsdram1.Init.SDBank = FMC_SDRAM_BANK1;
+  hsdram1.Init.ColumnBitsNumber = FMC_SDRAM_COLUMN_BITS_NUM_9;
+  hsdram1.Init.RowBitsNumber = FMC_SDRAM_ROW_BITS_NUM_12;
+  hsdram1.Init.MemoryDataWidth = FMC_SDRAM_MEM_BUS_WIDTH_16;
+  hsdram1.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
+  hsdram1.Init.CASLatency = FMC_SDRAM_CAS_LATENCY_3;
+  hsdram1.Init.WriteProtection = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
+  hsdram1.Init.SDClockPeriod = FMC_SDRAM_CLOCK_PERIOD_3;
+  hsdram1.Init.ReadBurst = FMC_SDRAM_RBURST_DISABLE;
+  hsdram1.Init.ReadPipeDelay = FMC_SDRAM_RPIPE_DELAY_2;
+  /* SdramTiming */
+  SdramTiming.LoadToActiveDelay = 16;
+  SdramTiming.ExitSelfRefreshDelay = 16;
+  SdramTiming.SelfRefreshTime = 16;
+  SdramTiming.RowCycleDelay = 16;
+  SdramTiming.WriteRecoveryTime = 16;
+  SdramTiming.RPDelay = 16;
+  SdramTiming.RCDDelay = 16;
+
+  if (HAL_SDRAM_Init(&hsdram1, &SdramTiming) != HAL_OK)
+  {
+    Error_Handler( );
+  }
+
+  /* USER CODE BEGIN FMC_Init 2 */
+
+  /* USER CODE END FMC_Init 2 */
+}
+
 /**
   * @brief GPIO Initialization Function
   * @param None
@@ -263,10 +439,15 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOE, DEBUG_LED_Pin|RGBLED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : USER1_Pin USER2_Pin USER3_Pin */
   GPIO_InitStruct.Pin = USER1_Pin|USER2_Pin|USER3_Pin;
@@ -280,6 +461,13 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(DEBUG_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : RGBLED_Pin */
+  GPIO_InitStruct.Pin = RGBLED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(RGBLED_GPIO_Port, &GPIO_InitStruct);
 
 }
 
