570ee59ced888d2145f91fe06e714ac877b95901
Brad Grantham <grantham@plunk.org>
2020-12-04 17:05:10 -0800

    checkpoint apple2e emulator hack

---
+++ rosa.peripheral.test/interface_text.cpp
@@ -0,0 +1,232 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <deque>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <unistd.h>
+
+#include "interface.h"
+
+using namespace std;
+
+namespace APPLE2Einterface
+{
+
+DisplayMode display_mode = TEXT;
+int display_page = 0; // Apple //e page minus 1 (so 0,1 not 1,2)
+bool mixed_mode = false;
+bool vid80 = false;
+bool altchar = false;
+
+static constexpr int text_page1_base = 0x400;
+static constexpr int text_page2_base = 0x800;
+static constexpr int text_page_size = 0x400;
+static constexpr int hires_page1_base = 0x2000;
+static constexpr int hires_page2_base = 0x4000;
+static constexpr int hires_page_size = 8192;
+
+extern "C" {
+extern int WozModePage;
+extern enum DisplayMode WozModeDisplayMode;
+extern int WozModeMixed;
+extern uint8_t WozModeHGRBuffers[2][8192];
+extern uint8_t WozModeTextBuffers[2][1024];
+};
+
+
+deque<event> event_queue;
+
+bool event_waiting()
+{
+    return event_queue.size() > 0;
+}
+
+event dequeue_event()
+{
+    if(event_waiting()) {
+        event e = event_queue.front();
+        event_queue.pop_front();
+        return e;
+    } else
+        return {NONE, 0};
+}
+
+tuple<float,bool> get_paddle(int num)
+{
+    if(num < 0 || num > 3)
+        return make_tuple(-1, false);
+    return make_tuple(0, false);
+}
+
+void start(bool run_fast, bool add_floppies, bool floppy0_inserted, bool floppy1_inserted)
+{
+    event_queue.push_back({KEYDOWN, CAPS_LOCK});
+}
+
+void apply_writes(void);
+
+void poll_keyboard()
+{
+    int i;
+    char c;
+
+    while((i = read(0, &c, 1)) != -1) {
+        bool control = false;
+        int ch;
+        if(c == '\r') {
+            ch = ENTER;
+        } else if(c >= 1 && c<= 26) {
+            control = true;
+            ch = 'A' + c - 1;
+        } else if(c >= 'a' && c<= 'z') {
+            ch = 'A' + c - 'a';
+        } else {
+            ch = c;
+        }
+        if(control)
+            event_queue.push_back({KEYDOWN, LEFT_CONTROL});
+        event_queue.push_back({KEYDOWN, ch});
+        event_queue.push_back({KEYUP, ch});
+        if(control)
+            event_queue.push_back({KEYUP, LEFT_CONTROL});
+    }
+    if (errno == EAGAIN) {
+        // Nothing to read.
+    } else {
+        printf("Got error reading from keyboard: %d\n\r", errno);
+        exit(1);
+    }
+}
+
+void iterate(const ModeHistory& history, unsigned long long current_byte, float megahertz)
+{
+    apply_writes();
+    if(history.size() > 0) {
+        const auto& lastModePoint = history.back();
+        const auto& [when, lastMode] = lastModePoint;
+        WozModeDisplayMode = lastMode.mode;
+        WozModeMixed = lastMode.mixed;
+        WozModePage = lastMode.page;
+    }
+
+    // poll_keyboard();
+}
+
+void shutdown()
+{
+}
+
+void set_switches(DisplayMode mode_, bool mixed, int page, bool vid80_, bool altchar_)
+{
+    display_mode = mode_;
+    mixed_mode = mixed;
+    display_page = page;
+    vid80 = vid80_;
+    altchar = altchar_;
+
+    // XXX
+    static bool altchar_warned = false;
+    if(altchar && !altchar_warned) {
+        fprintf(stderr, "Warning: ALTCHAR activated, is not implemented\n");
+        altchar_warned = true;
+    }
+}
+
+extern const int text_row_base_offsets[24];
+
+typedef pair<int, bool> address_auxpage;
+map<address_auxpage, unsigned char> writes;
+int collisions = 0;
+
+void write2(int addr, bool aux, unsigned char data)
+{
+    // We know text page 1 and 2 are contiguous
+    if((addr >= text_page1_base) && (addr < text_page2_base + text_page_size)) {
+        int page = (addr >= text_page2_base) ? 1 : 0;
+        int within_page = addr - text_page1_base - page * text_page_size;
+        WozModeTextBuffers[page][within_page] = data;
+
+    } else if(((addr >= hires_page1_base) && (addr < hires_page1_base + hires_page_size)) || ((addr >= hires_page2_base) && (addr < hires_page2_base + hires_page_size))) {
+
+        int page = (addr < hires_page2_base) ? 0 : 1;
+        int within_page = addr - hires_page1_base - page * hires_page_size;
+        WozModeHGRBuffers[page][within_page] = data;
+    }
+}
+
+void apply_writes(void)
+{
+    for(auto it : writes) {
+        int addr;
+        bool aux;
+        tie(addr, aux) = it.first;
+        write2(addr, aux, it.second); 
+    }
+    writes.clear();
+    collisions = 0;
+}
+
+bool write(int addr, bool aux, unsigned char data)
+{
+    // We know text page 1 and 2 are contiguous
+    if((addr >= text_page1_base) && (addr < text_page2_base + text_page_size)) {
+
+        if(writes.find({addr, aux}) != writes.end())
+            collisions++;
+        writes[{addr, aux}] = data;
+        return true;
+
+    } else if(((addr >= hires_page1_base) && (addr < hires_page1_base + hires_page_size)) || ((addr >= hires_page2_base) && (addr < hires_page2_base + hires_page_size))) {
+
+        if(writes.find({addr, aux}) != writes.end())
+            collisions++;
+        writes[{addr, aux}] = data;
+        return true;
+    }
+    return false;
+}
+
+
+const int text_row_base_offsets[24] =
+{
+    0x000,
+    0x080,
+    0x100,
+    0x180,
+    0x200,
+    0x280,
+    0x300,
+    0x380,
+    0x028,
+    0x0A8,
+    0x128,
+    0x1A8,
+    0x228,
+    0x2A8,
+    0x328,
+    0x3A8,
+    0x050,
+    0x0D0,
+    0x150,
+    0x1D0,
+    0x250,
+    0x2D0,
+    0x350,
+    0x3D0,
+};
+
+
+void show_floppy_activity(int number, bool activity)
+{
+}
+
+void enqueue_audio_samples(char *buf, size_t sz)
+{
+}
+
+};
---
+++ rosa.peripheral.test/syscalls.c
@@ -0,0 +1,334 @@
+/* Support files for GNU libc.  Files in the system namespace go here.
+   Files in the C namespace (ie those that do not start with an
+   underscore) go in .c.  */
+
+#include <_ansi.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/times.h>
+#include <errno.h>
+#include <reent.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#undef USE_FATFS
+
+#ifdef USE_FATFS
+#include "ff.h"
+#endif
+
+#undef errno
+extern int errno;
+
+#undef FreeRTOS
+#define MAX_STACK_SIZE 0x200
+
+extern int __io_putchar(int ch) __attribute__((weak));
+extern int __io_getchar(void) __attribute__((weak));
+
+
+caddr_t _sbrk(int incr)
+{
+	static char *heap_end;
+	extern char heap_low;
+	extern char heap_top;
+	char *prev_heap_end;
+
+	if (heap_end == 0)
+		heap_end = &heap_low;
+
+	prev_heap_end = heap_end;
+
+	if (heap_end + incr > &heap_top)
+	{
+//		write(1, "Heap and stack collision\n", 25);
+//		abort();
+		errno = ENOMEM;
+		return (caddr_t) -1;
+	}
+
+	heap_end += incr;
+
+	return (caddr_t) prev_heap_end;
+}
+
+uint32_t HAL_GetTick();
+
+/*
+ * _gettimeofday primitive (Stub function)
+ * */
+int _gettimeofday (struct timeval * tp, struct timezone * tzp)
+{
+  /* Return fixed data for the timezone.  */
+  if (tzp)
+    {
+      tzp->tz_minuteswest = 0;
+      tzp->tz_dsttime = 0;
+    }
+    if(tp) 
+    {
+        uint32_t millis = HAL_GetTick();
+        tp->tv_sec = millis / 1000;
+        tp->tv_usec = (millis % 1000) * 1000;
+    }
+
+  return 0;
+}
+
+void initialise_monitor_handles()
+{
+}
+
+int _getpid(void)
+{
+	return 1;
+}
+
+int _kill(int pid, int sig)
+{
+	errno = EINVAL;
+	return -1;
+}
+
+void _exit (int status)
+{
+	_kill(status, -1);
+	while (1) {}
+}
+
+#ifdef USE_FATFS
+
+#define MAX_FILES 4
+enum { FD_OFFSET = 3};
+static FIL files[MAX_FILES];    /* starting with fd=3, so fd 3 through 3 + MAX_FILES - 1 */
+static int filesOpened[MAX_FILES];
+
+#endif /* USE_FATFS */
+
+int _write(int file, char *ptr, int len)
+{
+    if(file < 0) { errno =  EINVAL; return -1; }
+
+    if((file == 0) || (file == 1) || (file == 2)) {
+	int DataIdx;
+
+		for (DataIdx = 0; DataIdx < len; DataIdx++)
+		{
+		   __io_putchar( *ptr++ );
+		}
+	return len;
+    } else {
+#ifdef USE_FATFS
+        int myFile = file - FD_OFFSET;
+        if(!filesOpened[myFile]) {
+            printf("XXX write: file not opened\n");
+            errno = EBADF;
+            return -1;
+        }
+        unsigned int wrote;
+        FRESULT result = f_write(&files[myFile], ptr, len, &wrote);
+        if(result != FR_OK) {
+            printf("XXX write: file result %d\n", result);
+            errno = EIO;
+            return -1;
+        }
+        return wrote;
+#else /* not USE_FATFS */
+        errno = EIO;
+        return -1;
+#endif /* USE_FATFS */
+    }
+}
+
+int _close(int file)
+{
+#ifdef USE_FATFS
+    int myFile = file - FD_OFFSET;
+    if(!filesOpened[myFile]) {
+        errno = EBADF;
+        return -1;
+    }
+    f_close(&files[myFile]);
+    filesOpened[myFile] = 0;
+#endif /* USE_FATFS */
+    return 0;
+}
+
+int _fstat(int file, struct stat *st)
+{
+	st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _isatty(int file)
+{
+	return 1;
+}
+
+int _lseek(int file, int ptr, int dir)
+{
+    if(file < 0) { errno =  EINVAL; return -1; }
+
+    if((file == 0) || (file == 1) || (file == 2)) {
+	return 0;
+    } else {
+#ifdef USE_FATFS
+        int myFile = file - FD_OFFSET;
+        if(!filesOpened[myFile]) {
+            printf("XXX lseek: file not opened %d\n", myFile);
+            errno = EBADF;
+            return -1;
+        }
+        FRESULT result;
+        if(dir == SEEK_SET) {
+            result = f_lseek(&files[myFile], ptr);
+        } else if(dir == SEEK_CUR) {
+            result = f_lseek(&files[myFile], ptr + f_tell(&files[myFile]));
+        } else /* SEEK_END */ {
+            result = f_lseek(&files[myFile], f_size(&files[myFile]) - 1 - ptr);
+        }
+        if(result != FR_OK) {
+            printf("XXX lseek: result not OK %d\n", result);
+            errno = EIO;
+            return -1;
+        }
+        return f_tell(&files[myFile]);
+#else /* not USE_FATFS */
+        errno = EIO;
+        return -1;
+#endif /* USE_FATFS */
+    }
+}
+
+int _read(int file, char *ptr, int len)
+{
+    if(file < 0) { errno =  EINVAL; return -1; }
+
+    if((file == 0) || (file == 1) || (file == 2)) {
+	int DataIdx;
+
+	for (DataIdx = 0; DataIdx < len; DataIdx++)
+	{
+	  *ptr++ = __io_getchar();
+	}
+        return len;
+    } else {
+#ifdef USE_FATFS
+        int myFile = file - FD_OFFSET;
+        if(!filesOpened[myFile]) {
+            printf("XXX read: file not opened %d\n", myFile);
+            errno = EBADF;
+            return -1;
+        }
+        unsigned int wasRead;
+        FRESULT result = f_read(&files[myFile], ptr, len, &wasRead);
+        if(result != FR_OK) {
+            printf("XXX read: result not OK %d\n", result);
+            errno = EIO;
+            return -1;
+        }
+        return wasRead;
+#else /* not USE_FATFS */
+        errno = EIO;
+        return -1;
+#endif /* USE_FATFS */
+    }
+}
+
+int _open(char *path, int flags, ...)
+{
+    if(path == NULL) {
+        errno = EFAULT;
+        return -1;
+    }
+
+#ifdef USE_FATFS
+    int which = 0;
+    while(which < MAX_FILES && filesOpened[which]) {
+        which++;
+    }
+    if(which >= MAX_FILES) {
+        errno = ENFILE;
+        return -1;
+    }
+
+    int FatFSFlags = 0;
+
+    if((flags & O_ACCMODE) == O_RDONLY) {
+        FatFSFlags |= FA_READ | FA_OPEN_EXISTING;
+    } else if((flags & O_ACCMODE) == O_WRONLY) {
+        FatFSFlags |= FA_WRITE;
+    } else if((flags & O_ACCMODE) == O_RDWR) {
+        FatFSFlags |= FA_WRITE | FA_READ;
+    }
+
+    if(flags & O_APPEND) {
+        FatFSFlags |= FA_OPEN_APPEND;
+    }
+    if(flags & O_CREAT) {
+        FatFSFlags |= FA_CREATE_NEW;
+    }
+    if(flags & O_TRUNC) {
+        FatFSFlags |= FA_CREATE_ALWAYS;
+    }
+    errno = 0;
+    FRESULT result = f_open (&files[which], path, FatFSFlags);
+    if(result) {
+        printf("XXX open couldn't open \"%s\" for reading, FatFS result %d\n", path, result);
+        errno = EIO;
+        return -1;
+    }
+    filesOpened[which] = 1;
+
+    return which + FD_OFFSET;
+#else /* not USE_FATFS */
+    errno = EIO;
+    return -1;
+#endif /* USE_FATFS */
+}
+
+int _wait(int *status)
+{
+	errno = ECHILD;
+	return -1;
+}
+
+int _unlink(char *name)
+{
+	errno = ENOENT;
+	return -1;
+}
+
+int _times(struct tms *buf)
+{
+	return -1;
+}
+
+int _stat(char *file, struct stat *st)
+{
+	st->st_mode = S_IFCHR;
+	return 0;
+}
+
+int _link(char *old, char *new)
+{
+	errno = EMLINK;
+	return -1;
+}
+
+int _fork(void)
+{
+	errno = EAGAIN;
+	return -1;
+}
+
+int _execve(char *name, char **argv, char **env)
+{
+	errno = ENOMEM;
+	return -1;
+}
